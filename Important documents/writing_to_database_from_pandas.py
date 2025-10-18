33fb8611b9910a593bbd56dc91a44d8dc32fb476
skdivvela <52740813+skdivvela@users.noreply.github.com>
2025-10-18 15:44:05 +0530

    Update writing_to_database_from_pandas.py

--- Important documents/writing_to_database_from_pandas.py
+++ Important documents/writing_to_database_from_pandas.py
@@ -15,6 +15,6 @@ df = pd.DataFrame({
 engine = create_engine('mysql+pymysql://user:password@localhost:3306/mydatabase')
 
 # Write DataFrame to table 'employees'
-df.to_sql('employees', engine, if_exists='replace', index=False)
+df.to_sql('employees', engine, if_exists='append', index=False)
 
 print("✅ DataFrame written to SQL table 'employees' successfully!")
